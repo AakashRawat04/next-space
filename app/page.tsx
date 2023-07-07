09b4c8dade354244e64406cf91961a89f984ea88
Ash <01tripster@gmail.com>
2023-07-08 01:23:03 +0530

    refactor: improve page and add logo svg png

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
-import Image from "next/image";
+import styles from "./page.module.css";
 
 export default function Home() {
-	return (
-		<main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
-	);
+	return <main></main>;
 }
