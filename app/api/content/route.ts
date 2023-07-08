c0de63c809b4a6dd91b885b130d24065a8807577
Ash <01tripster@gmail.com>
2023-07-09 01:19:56 +0530

    feat: add auth in apis

--- app/api/content/route.ts
+++ app/api/content/route.ts
@@ -31,8 +31,12 @@ const posts = [
 	},
 ];
 
+import { getServerSession } from "next-auth";
 import { NextResponse } from "next/server";
 
 export async function GET() {
+	const session = await getServerSession();
+	console.log("session:", session);
+
 	return NextResponse.json(posts);
 }
